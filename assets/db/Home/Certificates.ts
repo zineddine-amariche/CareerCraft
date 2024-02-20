58f03fa47f20c186665c5983f68abc6f977582b6
azinedine <amarichezineddine@gmail.com>
2024-02-20 09:57:17 +0100

    Refactor : listInfo updated

---
+++ assets/db/Home/Certificates.ts
@@ -0,0 +1,85 @@
+// import { Masahif as MasahifType } from "app/quran-helpers/quranData"
+
+export const Certificates = {
+  certificate1: {
+    enabled: true,
+    id: "experience1",
+    name: "certificate1",
+    countingType: "",
+    ext: "",
+    url: "", 
+    local: "",
+    extraStartPages: 0,
+    meta: {
+      name: {
+        ar: "مصحف شريفي الثاني",
+        en: "Mushaf Shirifi II",
+        fr: "Mushaf Shirifi II",
+      },
+      job: {
+        ar: "رواية ورش عن الإمام نافع من طريق الأزرق",
+        en: "Warsh from Imam Nafi' via Azraq",
+        fr: "Warsh de l'Imam Nafi' via Azraq",
+      },
+      description: {
+        ar: "طبعة المؤسسة الوطنية للفنون المطبعية",
+        en: "National Printing Arts Institution Edition",
+        fr: "Édition de l'Institution nationale des arts d'impression",
+      },
+    },
+  },
+  certificate2: {
+    enabled: true,
+    id: "experience2",
+    name: "certificate2",
+    countingType: "",
+    ext: "",
+    url: "",
+    local: "",
+    extraStartPages: 3,
+    meta: {
+      name: {
+        ar: "مصحف دار الإمام مالك",
+        en: "Mushaf Dar Imam Malik",
+        fr: "Mushaf Dar Imam Malik",
+      },
+      job: {
+        ar: "رواية ورش عن الإمام نافع من طريق الأزرق",
+        en: "Warsh from Imam Nafi' via Azraq",
+        fr: "Warsh de l'Imam Nafi' via Azraq",
+      },
+      description: {
+        ar: "طبعة دار الإمام مالك",
+        en: "Dar Imam Malik Edition",
+        fr: "Édition de Dar Imam Malik",
+      },
+    },
+  },
+  certificate3: {
+    enabled: true,
+    id: "experience3",
+    name: "certificate3",
+    countingType: "",
+    ext: "",
+    url: "",
+    local: "",
+    extraStartPages: 3,
+    meta: {
+      name: {
+        ar: "مصحف دار الإمام مالك",
+        en: "Mushaf Dar Imam Malik",
+        fr: "Mushaf Dar Imam Malik",
+      },
+      job: {
+        ar: "رواية ورش عن الإمام نافع من طريق الأزرق",
+        en: "Warsh from Imam Nafi' via Azraq",
+        fr: "Warsh de l'Imam Nafi' via Azraq",
+      },
+      description: {
+        ar: "طبعة دار الإمام مالك",
+        en: "Dar Imam Malik Edition",
+        fr: "Édition de Dar Imam Malik",
+      },
+    },
+  },
+}
---
+++ assets/db/Home/Experiences.ts
@@ -0,0 +1,85 @@
+// import { Masahif as MasahifType } from "app/quran-helpers/quranData"
+
+export const Experiences = {
+  experience1: {
+    enabled: true,
+    id: "experience1",
+    name: "experience1",
+    countingType: "",
+    ext: "",
+    url: "", 
+    local: "",
+    extraStartPages: 0,
+    meta: {
+      name: {
+        ar: "مصحف شريفي الثاني",
+        en: "Mushaf Shirifi II",
+        fr: "Mushaf Shirifi II",
+      },
+      job: {
+        ar: "رواية ورش عن الإمام نافع من طريق الأزرق",
+        en: "Warsh from Imam Nafi' via Azraq",
+        fr: "Warsh de l'Imam Nafi' via Azraq",
+      },
+      description: {
+        ar: "طبعة المؤسسة الوطنية للفنون المطبعية",
+        en: "National Printing Arts Institution Edition",
+        fr: "Édition de l'Institution nationale des arts d'impression",
+      },
+    },
+  },
+  experience2: {
+    enabled: true,
+    id: "experience2",
+    name: "experience2",
+    countingType: "",
+    ext: "",
+    url: "",
+    local: "",
+    extraStartPages: 3,
+    meta: {
+      name: {
+        ar: "مصحف دار الإمام مالك",
+        en: "Mushaf Dar Imam Malik",
+        fr: "Mushaf Dar Imam Malik",
+      },
+      job: {
+        ar: "رواية ورش عن الإمام نافع من طريق الأزرق",
+        en: "Warsh from Imam Nafi' via Azraq",
+        fr: "Warsh de l'Imam Nafi' via Azraq",
+      },
+      description: {
+        ar: "طبعة دار الإمام مالك",
+        en: "Dar Imam Malik Edition",
+        fr: "Édition de Dar Imam Malik",
+      },
+    },
+  },
+  experience3: {
+    enabled: true,
+    id: "experience3",
+    name: "experience3",
+    countingType: "",
+    ext: "",
+    url: "",
+    local: "",
+    extraStartPages: 3,
+    meta: {
+      name: {
+        ar: "مصحف دار الإمام مالك",
+        en: "Mushaf Dar Imam Malik",
+        fr: "Mushaf Dar Imam Malik",
+      },
+      job: {
+        ar: "رواية ورش عن الإمام نافع من طريق الأزرق",
+        en: "Warsh from Imam Nafi' via Azraq",
+        fr: "Warsh de l'Imam Nafi' via Azraq",
+      },
+      description: {
+        ar: "طبعة دار الإمام مالك",
+        en: "Dar Imam Malik Edition",
+        fr: "Édition de Dar Imam Malik",
+      },
+    },
+  },
+}
